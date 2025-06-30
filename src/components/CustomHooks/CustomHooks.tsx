f4a223f473a9b7b630cf4682d84b6768343343b8
Master1624 <jdpa83@gmail.com>
2025-06-30 17:46:50 -0500

    feat: Custom hooks creation

--- src/components/CustomHooks/CustomHooks.tsx
+++ src/components/CustomHooks/CustomHooks.tsx
@@ -1,10 +1,29 @@
-const UserComponent = () => {
+import { useState } from "react";
+
+function useLoading(initialLoading: boolean = false) {
+  const [isLoading, setisLoading] = useState<boolean>(initialLoading);
+
+  return { isLoading, setisLoading };
+}
+
+type UserProps = {
+  name: string;
+};
+
+const UserComponent: React.FC<UserProps> = ({ name }) => {
+  const { isLoading, setisLoading } = useLoading(false);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
+      <p>Hello, {name}</p>
+      <button onClick={() => setisLoading(true)}>Simulate Loading</button>
     </div>
   );
 };
 
 export const ParentComponent = () => {
-  return <UserComponent />;
+  return <UserComponent name="John Davoe" />;
 };
