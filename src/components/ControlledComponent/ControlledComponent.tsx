4b1735795750f4f0ea09c927559c559363682b36
Master1624 <jdpa83@gmail.com>
2025-06-30 17:49:17 -0500

    feat: Controlled and Uncontrolled components

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 export const ControlledInput = () => {
+  const [value, setValue] = useState<string>("");
+  
   return (
     <div>
+      <input
+        type="text"
+        value={value}
+        placeholder="Ingresa el código del cupón. Ej: 10OFF"
+        onChange={(e) => {
+          const newValue = e.target.value;
+          setValue(newValue);
+        }}
+      />
+      <p>
+        Cupón de descuento: <b>{value}</b>
+      </p>
     </div>
   );
 };
--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,20 @@
+import { useRef } from "react";
+
 export const UncontrolledInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleSubmit = () => {
+    if (inputRef.current) {
+      alert(`Nuevo producto en el carrito: ${inputRef.current.value}✅`);
+    }
+  };
   return (
     <div>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Nombre del producto. Ej: Manzana"
+      />
+      <button onClick={handleSubmit}>Añadir al carrito</button>
     </div>
   );
 };
