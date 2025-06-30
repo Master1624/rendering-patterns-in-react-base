cd8711c1d254002f2d6cd571190254b0e931b534
Master1624 <jdpa83@gmail.com>
2025-06-30 17:40:31 -0500

    feat: Compound components pattern

--- src/components/CompoundComponents/CompoundComponents.tsx
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -1,6 +1,12 @@
+import Tab from "./Tab";
+import Tabs from "./Tabs";
+
 export const CompoundComponents = () => {
   return (
-    <>
-    </>
+    <Tabs>
+      <Tab label="Jiraffe">🦒</Tab>
+      <Tab label="Dolphin">🐬</Tab>
+      <Tab label="Crocodile">🐊</Tab>
+    </Tabs>
   );
 };
